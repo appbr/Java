c4f158020596f1666c68cff51d7feba1a03def18
appbr <telles.jadlog@gmail.com>
2022-11-01 13:28:31 -0300

    class conection

---
+++ Crud_Java/src/conexaoDAO.java
@@ -0,0 +1,22 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+public class conexaoDAO {
+    public Connection conectaBD(){
+        Connection conn = null;
+        
+        try {
+            String url = "jdbc:mysql://sql10.freesqldatabase.com:3306/sql10531594?user=sql10531594&password=itPIiLlL1t";
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null,"ConexaoDAO" + erro.getMessage());
+        }
+        
+        return conn;
+    }
+    
+}
